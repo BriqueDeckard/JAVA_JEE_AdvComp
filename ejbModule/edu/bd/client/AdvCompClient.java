ff53bc4d52941c43a3d5f4553fb2ae7913668068
Brique Deckard <pierre.4ntoine@outlook.fr>
2021-03-28 12:35:16 +0200

    started client

---
+++ ejbModule/edu/bd/client/AdvCompClient.java
@@ -0,0 +1,18 @@
+// File AdvCompClient.java - No copyright - 28 mars 2021
+package edu.bd.client;
+
+/**
+ * TODO Fill type utility
+ * @author Brique DECKARD
+ *
+ */
+public class AdvCompClient {
+
+    /**
+     * Constructor for AdvCompClient
+     *
+     */
+    public AdvCompClient() {
+    }
+    // TODO : Fill class body
+}
