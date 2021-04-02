86d3ffed5179a1a19ecd48750aa29bb821e52910
Brique Deckard <pierre.4ntoine@outlook.fr>
2021-04-02 11:50:00 +0200

    Commenté UtilisateurDaoSql_Mock et implémenté UtilisateurDaoSql

--- ejbModule/edu/bd/advcomp/authentification/dao/impl/UtilisateurDaoSql.java
+++ ejbModule/edu/bd/advcomp/authentification/dao/impl/UtilisateurDaoSql.java
@@ -1,106 +1,156 @@
-/*
- * // File UtilisateurDaoSql.java - No copyright - 23 mars 2021 package
- * edu.bd.advcomp.authentification.dao.impl;
+// File UtilisateurDaoSql.java - No copyright - 23 mars 2021 package
+package edu.bd.advcomp.authentification.dao.impl;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import edu.bd.advcomp.AdvcompException;
+import edu.bd.advcomp.DEV_CONFIG;
+import edu.bd.advcomp.authentification.Role;
+import edu.bd.advcomp.authentification.dao.UtilisateurDao;
+import edu.bd.advcomp.authentification.entity.Utilisateur;
+import edu.bd.advcomp.authentification.entity.impl.UtilisateurImpl;
+
+/**
+ * Implémentation du DAO pour la maintenance des {@link Utilisateur}
  * 
- * import java.util.Arrays; import java.util.List;
- * 
- * import javax.ejb.Stateless;
- * 
- * import edu.bd.advcomp.AdvcompException; import edu.bd.advcomp.DEV_CONFIG;
- * import edu.bd.advcomp.authentification.Role; import
- * edu.bd.advcomp.authentification.dao.UtilisateurDao; import
- * edu.bd.advcomp.authentification.entity.Utilisateur; import
- * edu.bd.advcomp.authentification.entity.impl.UtilisateurImpl;
- * 
- *//**
-    * Implémentation du DAO pour la maintenance des {@link Utilisateur}
-    * 
-    * @author Brique DECKARD
-    *
-    */
-/*
- * // TODO : Or stateful ?
- * 
- * @Stateless public class UtilisateurDaoSql implements UtilisateurDao {
- * 
- *//**
-    * Mock
-    */
-/*
- * private Utilisateur utilisateurToto = new UtilisateurImpl("toto", "secret",
- * "Bob", "1 rue Tutu", Role.CLIENT);
- * 
- *//**
-    * Constructor for UtilisateurDaoSql
-    */
-/*
- * public UtilisateurDaoSql() { }
- * 
- *//**
-    * See @see edu.bd.framework.persistence.EntityDao#create(java.lang.Object)
-    * 
-    * @param entity
-    * @return
-    * @throws AdvcompException
-    */
-/*
- * @Override public Utilisateur create(Utilisateur entity) throws
- * AdvcompException { return entity; }
- * 
- *//**
-    * See @see edu.bd.framework.persistence.EntityDao#retrieve(java.lang.Object)
-    * 
-    * @param id
-    * @return
-    * @throws AdvcompException
-    */
-/*
- * @Override public Utilisateur retrieve(String id) throws AdvcompException { //
- * TODO : Implements real persistence
- * //System.out.println(this.getClass().getName() + " Retrieve " + id); if
- * (DEV_CONFIG.USERNAME.toString().equals(id)) { return utilisateurToto; }
- * return null; }
- * 
- *//**
-    * See @see edu.bd.framework.persistence.EntityDao#update(java.lang.Object)
-    * 
-    * @param entity
-    * @return
-    * @throws AdvcompException
-    */
-/*
- * @Override public Utilisateur update(Utilisateur entity) throws
- * AdvcompException { return entity; }
- * 
- *//**
-    * See @see edu.bd.framework.persistence.EntityDao#delete(java.lang.Object)
-    * 
-    * @param entity
-    * @throws AdvcompException
-    */
-/*
- * @Override public void delete(Utilisateur entity) throws AdvcompException { //
- * TODO Fill method utility. }
- * 
- *//**
-    * See @see edu.bd.framework.persistence.EntityDao#getNew()
-    * 
-    * @return
-    * @throws AdvcompException
-    */
-/*
- * @Override public Utilisateur getNew() throws AdvcompException { return new
- * UtilisateurImpl(); }
- * 
- *//**
-    * See @see edu.bd.framework.persistence.EntityDao#retrieveAll()
-    *
-    * @return
-    * @throws AdvcompException
-    * @throws Exception
-    *//*
-       * @Override public List<Utilisateur> retrieveAll() throws AdvcompException,
-       * Exception { return Arrays.asList(utilisateurToto); }
-       * 
-       * }
-       */
+ * @author Brique DECKARD
+ *
+ */
+
+@Stateless
+public class UtilisateurDaoSql implements UtilisateurDao {
+
+    @PersistenceContext(unitName = "advcomp")
+    EntityManager em;
+
+    /**
+     * Constructor for UtilisateurDaoSql
+     */
+
+    public UtilisateurDaoSql() {
+    }
+
+    /**
+     * See @see edu.bd.framework.persistence.EntityDao#create(java.lang.Object)
+     * 
+     * @param entity
+     * @return
+     * @throws AdvcompException
+     */
+
+    @Override
+    public Utilisateur create(Utilisateur entity) throws AdvcompException {
+	System.out.println("CREATE Utilisateur " + entity.getLogin());
+	if (entity == null) {
+	    throw new AdvcompException("ERROR : Entity is null");
+	}
+	try {
+	    em.persist(entity);
+	    return entity;
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    throw new AdvcompException("ERROR : Echec persistance");
+	}
+
+    }
+
+    /**
+     * See @see edu.bd.framework.persistence.EntityDao#retrieve(java.lang.Object)
+     * 
+     * @param id
+     * @return
+     * @throws AdvcompException
+     */
+
+    @Override
+    public Utilisateur retrieve(String id) throws AdvcompException {
+	System.out.println("RETRIEVE Utilisateur " + id);
+	if (id.isEmpty()) {
+	    throw new AdvcompException("ERROR : Id is null");
+	}
+	try {
+	    Utilisateur user = em.find(UtilisateurImpl.class, id);
+	    return user;
+
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    throw new AdvcompException("ERROR : Echec persistance");
+	}
+
+    }
+
+    /**
+     * See @see edu.bd.framework.persistence.EntityDao#update(java.lang.Object)
+     * 
+     * @param entity
+     * @return
+     * @throws AdvcompException
+     */
+
+    @Override
+    public Utilisateur update(Utilisateur entity) throws AdvcompException {
+	System.out.println("UPDATE Utilisateur " + entity.getLogin());
+	if (entity == null) {
+	    throw new AdvcompException("ERROR : Entity is null");
+	}
+	try {
+	    em.merge(entity);
+	    return entity;
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    throw new AdvcompException("ERROR : Echec persistance");
+	}
+
+    }
+
+    /**
+     * See @see edu.bd.framework.persistence.EntityDao#delete(java.lang.Object)
+     * 
+     * @param entity
+     * @throws AdvcompException
+     */
+
+    @Override
+    public void delete(Utilisateur entity) throws AdvcompException {
+	System.out.println("DELETE Utilisateur " + entity.getLogin());
+	if (entity == null) {
+	    throw new AdvcompException("ERROR : Entity is null");
+	}
+	try {
+	    em.remove(entity);
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    throw new AdvcompException("ERROR : Echec suppression");
+	}
+    }
+
+    /**
+     * See @see edu.bd.framework.persistence.EntityDao#getNew()
+     * 
+     * @return
+     * @throws AdvcompException
+     */
+
+    @Override
+    public Utilisateur getNew() throws AdvcompException {
+	return new UtilisateurImpl();
+    }
+
+    /**
+     * See @see edu.bd.framework.persistence.EntityDao#retrieveAll()
+     *
+     * @return
+     * @throws AdvcompException
+     * @throws Exception
+     */
+    @Override
+    public List<Utilisateur> retrieveAll() throws AdvcompException, Exception {
+	return null;
+    }
+
+}
